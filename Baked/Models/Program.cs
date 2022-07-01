250bb8d4e50ba05f444d2d55700d79a7cedb12c0
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-30 21:55:00 -0700

    outline for Program.cs

---
+++ Baked/Models/Program.cs
@@ -0,0 +1,14 @@
+using System; 
+using System.Collections.Generic; 
+using Baked.Models; 
+
+namespace Baked {
+  public class Program
+  {
+
+    public static void Main()
+    {
+      Console.WriteLine("Welcome To Pierre's Bakery"); 
+    }
+  }
+}
