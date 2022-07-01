b0d74e578ce3de21f90cf7e0fd192c88930b44d5
Ryan Broadsword <rbroadsword@gmail.com>
2022-06-30 22:24:32 -0700

    rough layout of menu, and retieve order

--- Baked/Models/Program.cs
+++ Baked/Models/Program.cs
@@ -8,7 +8,33 @@ namespace Baked {
 
     public static void Main()
     {
-      Console.WriteLine("Welcome To Pierre's Bakery"); 
+      Console.WriteLine("   Welcome To Pierre's Bakery"); 
+      Console.WriteLine("            Menu      "); 
+      Console.WriteLine("           Bread:         "); 
+      Console.WriteLine("   Single:  $5           "); 
+      Console.WriteLine("   Double:  $10          "); 
+      Console.WriteLine("   * Buy 2 loaves of bread and receive 1 free");
+      Console.WriteLine("           Pastry:         "); 
+      Console.WriteLine("   One:      $2"); 
+      Console.WriteLine("   Three:    $5"); 
+      Console.WriteLine("   Four:     $7"); 
+      Console.WriteLine("   Five:     $9"); 
+      Console.WriteLine("   Six:      $10"); 
+      Console.WriteLine("   Ready to Order? (Y/N)"); 
+      string order = Console.ReadLine();
+      if (order == "Y" || order == "y")
+      {
+        Console.WriteLine("   How many loaves of bread would you like?"); 
+        string breadOrder = Console.ReadLine(); 
+        Console.WriteLine("   How many pastry would you like?"); 
+        string pastryOrder = Console.ReadLine(); 
+        Console.WriteLine("   You have ordered " + breadOrder + " loaves of bread"); 
+        Console.WriteLine("   You have ordered " + pastryOrder + " pastries"); 
+
+      } else 
+      {
+        Console.WriteLine("See you soon"); 
+      }
     }
   }
 }
