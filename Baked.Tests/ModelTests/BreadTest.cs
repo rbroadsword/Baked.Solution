c7d51008fc633490cfbd790e16de48dbffe00c3b
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-02 09:52:54 -0700

    wrote first BreadOrderTests

--- Baked.Tests/ModelTests/BreadTest.cs
+++ Baked.Tests/ModelTests/BreadTest.cs
@@ -1,12 +1,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Baked.Models; 
 
-// namespace Baked.Tests
-// {
-//   [TestClass]
-//   public class PastryOrderTests
-//   {
-//     [TestMethod]
-    
-//   }
-// }
+namespace Baked.Tests
+{
+  [TestClass]
+  public class BreadOrderTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreateBreadOrder_BreadOrder()
+    {
+      BreadOrder myBreadOrder = new BreadOrder(2); 
+      Assert.AreEqual(typeof(BreadOrder), myBreadOrder.GetType()); 
+    }
+  }
+}
