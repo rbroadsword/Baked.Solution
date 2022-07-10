292a34457b14aacfb26b8f79ebcba4a14a4fe778
Ryan Broadsword <rbroadsword@gmail.com>
2022-07-10 08:00:48 -0700

    refactor PastryCost()

--- Baked/Models/Pastry.cs
+++ Baked/Models/Pastry.cs
@@ -15,16 +15,13 @@ namespace Baked.Models
     public int PastryCost(int numberOfPastry)
     {
       int pastryTotal = 0; 
-      if (numberOfPastry > 6)
+      if (numberOfPastry >= 6)
       {
         int bySix = numberOfPastry/6; 
         int sixRemainder = numberOfPastry % 6; 
         pastryTotal = (bySix * 10) + ((sixRemainder * 2));  
 
-      } else if (numberOfPastry == 6)
-      {
-        pastryTotal = numberOfPastry * 2 -2; 
-      } else if (numberOfPastry >= 3 && numberOfPastry < 6) 
+      }  else if (numberOfPastry >= 3 && numberOfPastry < 6) 
       {
         pastryTotal = numberOfPastry * 2 -1;
       } else 
